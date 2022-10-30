24e8d63aa5cf7768295c1736b3f83cdf7ea43010
Dan <evrondan99@gmail.com>
2022-10-30 22:12:27 +0200

    Bug- line 56 main caller reaserch 2

--- second_research/main_caller_r2.py
+++ second_research/main_caller_r2.py
@@ -43,25 +43,64 @@ def get_model_names_to_model_accuracy_scores_dict(X,y)->dict:
     return model_to_accuracy
 
 
+
+def get_y_column(X,y_name):
+    """Given a df X and a string y_name, returning a column y of it, corresponding to X rows"""
+
+    if y_name == '6-weeks_HDRS21_class': 
+        print("debug","y_name",y_name)
+        base = 'Baseline_HDRS21_totalscore'
+        end = '6-weeks_HDRS21_totalscore'
+        print(X.shape)
+        print(X[end])
+        X = X[X[end] !=  None] # problem here- trying to remove subjects with 'end' (6-week HDRS) that is None,
+        print(X.shape)
+    if y_name == '6-weeks_HARS_class':
+        base = 'Baseline_HARS_totalscore'
+        end = '6-weeks HARS_totalscore'  
+
+    X['%_change_rate'] = ((X[end] - X[base]) / X[base])* 100  # increase in % in HDRS 21 total score 
+    X[y_name] = X['%_change_rate'].apply(lambda change_rate: convert_change_rate_to_class(change_rate,X[end]))
+    print("debug")
+    print(X['y_name']) 
+
+def convert_change_rate_to_class(change_rate,week_6_score):
+    """Given change rate in % from baseline and week 6 score, return the class(state) due to that score and change rate """
+    state = None
+    print(change_rate,week_6_score)
+    if 0 <= week_6_score <= 7:
+        state = "remission" #also called normal state
+    else:
+        if change_rate < -50: # drop of 50 percents of more from the baseline
+            state = "responsive"
+        else:
+            state = "non_responsive"
+    return state
+
 bna_path = 'second_research\EYEC_Cordance_Gamma.csv'
 clinical_path = 'second_research\BW_clinical_data.csv'
 # Step 1: prepare X for model training
 X = numeric_df_initializer.generate_prediction_df(bna_path,clinical_path) #initial numeric-predicting data frame
-y_name = 'class_week_6'
-print(X.head)
+
 #X.to_csv('second_research/X_before prediction.csv',index = False)
-y = get_y_column(X,y_name) # implement
-X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.2,random_state = 0) #rs is seed
+#X_1 = X.copy()
+# X_1['y'] = X_1['age']  - X_1['age']
+# X_1['Z'] = X_1['age']  + X_1['y']
+# print(X_1['y'])
+# print(X_1['Z'])
 
+for y_name in ['6-weeks_HDRS21_class', '6-weeks_HARS_class']:
+    y = get_y_column(X.copy(),y_name) 
+    X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.2,random_state = 0) #rs is seed
 
-# Step 2: select classification model with k fold cross validation  
+    # Step 2: select classification model with k fold cross validation  
 
-X_train_copy = X_train.copy()  
-y_train_copy = y_train.copy()
-selected_model = select_best_model(X_train_copy, y_train_copy) # selecting best classification model with cross validation. using 80% of data
-# one shot preditciton:
-selected_model.fit(X_train,y_train)
-y_pred = selected_model.predict(X_test) # real 1 shot prediction
-selected_model_accuracy_score = metrics.accuracy_score(y_test,y_pred)
+    X_train_copy = X_train.copy()  
+    y_train_copy = y_train.copy()
+    selected_model = select_best_model(X_train_copy, y_train_copy) # selecting best classification model with cross validation. using 80% of data
+    # one shot preditciton:
+    selected_model.fit(X_train,y_train)
+    y_pred = selected_model.predict(X_test) # real 1 shot prediction
+    selected_model_accuracy_score = metrics.accuracy_score(y_test,y_pred)
 
 
--- second_research/numeric_df_initializer.py
+++ second_research/numeric_df_initializer.py
@@ -110,5 +110,6 @@ def generate_prediction_df(bna_path, clinical_path):
     restricted_cols_bna = set(['site','subject.elm_id','ageV1','taskData.elm_id','visit','taskData.acqSysId','taskClass.elm_id','key']) #'remove added to visits
     cols_to_drop = restricted_cols_bna.union(restricted_cols_clinical) #all the columns we want to remove 
     subjects_baseline = drop_cols(subjects_baseline, cols_to_drop) #dropping 
+    
     return subjects_baseline
     
