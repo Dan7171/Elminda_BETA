7bcb4699f7b3a0727659d53a0015d885f727f759
Dan7171 <evrondan99@gmail.com>
2023-04-29 20:39:18 +0300

    6c exhaustive

--- second_research/GSCVrunner.py
+++ second_research/GSCVrunner.py
@@ -644,13 +644,13 @@ if args['classification']:
     }
     # GRADIENT BOOSTING ( NO PCA  NO KBEST)
     param6c = {
-        'classifier__n_estimators': [5, 20, 35, 50, 65, 80, 95, 110, 125, 150, 200, 250, 350],
-        'classifier__learning_rate': [0.01, 0.2, 0.4],
-        'classifier__max_depth': [2,3, 5, 7, 9, 11],
-        'classifier__min_samples_split': [1, 3, 5, 8, 13, 19, 25, 33, 42, 55, 70],
-        'classifier__min_samples_leaf': [1, 3, 5, 8, 13, 19, 25, 33, 42, 55, 70],
+        # 'classifier__n_estimators': [5, 20, 35, 50, 65, 80, 95, 110, 125, 150, 200, 250, 350],
+        'classifier__learning_rate': [0.0001, 0.1],
+        'classifier__max_depth': [2,4, 8, 15, 25,33,50,70, 100,200],
+        'classifier__min_samples_split': [1, 5, 8, 19,  33, 47, 78, 100],
+        'classifier__min_samples_leaf': [1, 5, 8, 19,  33, 47, 78, 100],
         'classifier__max_features': ['auto', 'sqrt', None],
-        'classifier__subsample': [0.6, 0.75, 0.9, 1],
+        'classifier__subsample': [0.6, 0.95],
         "classifier": [clf6]
     }
 
@@ -862,7 +862,7 @@ for config in splitted_congifs:
 # *******************************
     if args['classification']:
         if args['lite_mode']:  # just for debugging. using one small grid
-            param_pipe_list = [[param5b, pipe5b]]
+            param_pipe_list = [[param6c, pipe6c]]
 # ********************************
         else:  # more than one model
             # pipe is represent the steps we want to execute, param represents which args we want to execute with
