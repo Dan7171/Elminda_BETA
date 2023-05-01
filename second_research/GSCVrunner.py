1e4331572abecebbab43f3ec5059e6ef7a06b9c6
Dan7171 <evrondan99@gmail.com>
2023-05-01 13:35:54 +0300

    4a

--- second_research/GSCVrunner.py
+++ second_research/GSCVrunner.py
@@ -566,12 +566,17 @@ if args['classification']:
         # 'classifier__criterion': 'entropy', 'classifier': DecisionTreeClassifier(criterion='entropy', max_depth=200, max_leaf_nodes=80,
         #                        min_samples_leaf=2, random_state=42)}"
 
-        "pca__n_components": range(40,56),
-        'classifier__max_leaf_nodes': range(77,105),
-        'classifier__max_depth': range(170,270,3),
+        # "{'pca__n_components': 52, 'classifier__min_samples_split': 2,
+        # 'classifier__min_samples_leaf': 2, 'classifier__max_leaf_nodes': 97,
+        # 'classifier__max_depth': 239, 'classifier__criterion': 'entropy',
+        # 'classifier': DecisionTreeClassifier(criterion='entropy', max_depth=239, max_leaf_nodes=97,
+        #                        min_samples_leaf=2, random_state=42)}"
+        "pca__n_components": range(49,56),
+        'classifier__max_leaf_nodes': range(89,105,2),
+        'classifier__max_depth': range(232,255,2),
         'classifier__criterion': ['entropy'],
-        'classifier__min_samples_split': range(2,7,2),
-        'classifier__min_samples_leaf': range(2,7,2),
+        'classifier__min_samples_split': range(2,5),
+        'classifier__min_samples_leaf': range(2,5),
         "classifier": [clf4]
     }
 
